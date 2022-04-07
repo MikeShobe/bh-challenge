ec5fc39447a8a428fb338b84828b90c0984b7696
Michael Smith <msmith@altair.com>
2022-04-07 02:46:26 -0500

    CSS

--- src/requests.ts
+++ src/requests.ts
@@ -1,4 +1,5 @@
 export const getData = async ({ options = {}}: any) => {
+  // TODO: Set up API endpoint
   const response = await fetch('<API ENDPOINT>', options);
   const data = await response.json();
 
